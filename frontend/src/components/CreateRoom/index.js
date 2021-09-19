ef2f7d66ca5737950ccfeaa4dfd5e2e1c1f1d019
Jacob Cambell <jacob@jacobcambell.com>
2021-09-19 17:13:46 -0600

    add fields and style to createroom

--- frontend/src/components/CreateRoom/index.js
+++ frontend/src/components/CreateRoom/index.js
@@ -2,7 +2,20 @@ import styles from './CreateRoom.module.css';
 
 const CreateRoom = () => {
     return (
-        <div>create room</div>
+        <div className="page">
+            <p className={styles.title}>Create a Room</p>
+
+            <p className={styles.label}>Room Name</p>
+            <input type="text" className={styles.field} />
+
+            <p className={styles.label}>Room Password</p>
+            <input type="password" className={styles.field} />
+
+            <p className={styles.label}>Your Name</p>
+            <input type="text" className={styles.field} />
+
+            <button className={styles.createBtn}>Create Room</button>
+        </div>
      );
 }
 
