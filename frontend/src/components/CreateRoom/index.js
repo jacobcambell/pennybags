9470b677ac5b2108005cc7e00c7ec787977e39f3
Jacob Cambell <jacob@jacobcambell.com>
2021-09-19 17:24:54 -0600

    Setup state, change state in fields, form handler

--- frontend/src/components/CreateRoom/index.js
+++ frontend/src/components/CreateRoom/index.js
@@ -1,20 +1,37 @@
+import { useState } from 'react';
 import styles from './CreateRoom.module.css';
+import io from 'socket.io-client';
 
 const CreateRoom = () => {
+
+    const socket = io('http://localhost:8000');
+
+    const [roomname, setRoomname] = useState();
+    const [roompassword, setRoompassword] = useState();
+    const [yourname, setYourname] = useState();
+
+    const handleForm = () => {
+        socket.emit('create-room', {
+            roomname,
+            roompassword,
+            yourname
+        });
+    }
+
     return (
         <div className="page">
             <p className={styles.title}>Create a Room</p>
 
             <p className={styles.label}>Room Name</p>
-            <input type="text" className={styles.field} />
+            <input onChange={(e) => { setRoomname(e.target.value) }} type="text" className={styles.field} />
 
             <p className={styles.label}>Room Password</p>
-            <input type="password" className={styles.field} />
+            <input onChange={(e) => { setRoompassword(e.target.value) }} type="password" className={styles.field} />
 
             <p className={styles.label}>Your Name</p>
-            <input type="text" className={styles.field} />
+            <input onChange={(e) => { setYourname(e.target.value) }} type="text" className={styles.field} />
 
-            <button className={styles.createBtn}>Create Room</button>
+            <button onClick={handleForm} className={styles.createBtn}>Create Room</button>
         </div>
      );
 }
