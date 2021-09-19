4ef86d735fcead80c07e93365d606c3bdc9357b7
Jacob Cambell <jacob@jacobcambell.com>
2021-09-19 17:04:22 -0600

    Install socketio and socketioclient

--- frontend/src/components/Home/index.js
+++ frontend/src/components/Home/index.js
@@ -2,6 +2,7 @@ import styles from './Home.module.css';
 import { Link } from 'react-router-dom';
 
 const Home = () => {
+
     return (
         <div className="page">
             <p className={styles.title}>Welcome to iMonopoly</p>
