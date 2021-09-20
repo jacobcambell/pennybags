b5be49274167c1f3b2e64085057e1289796fa233
Jacob Cambell <jacob@jacobcambell.com>
2021-09-20 17:07:30 -0600

    Create initial rooms array layout

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,17 @@ const io = require('socket.io')(server, {
     }
 });
 
+// rooms is an array of room objects, with each room object containing a name,
+// an array of player objects
+let rooms = [
+    // {
+    //     room_name: 'First Room',
+    //     players: [
+    //         {player_name: 'Mike', balance: 1500, secret: 'j18fh173n8'}
+    //     ]
+    // }
+];
+
 io.on('connection', socket => {
 
 });
