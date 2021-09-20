cfeeca70d2ea8650b26ec7852da031755739b3dd
Jacob Cambell <jacob@jacobcambell.com>
2021-09-20 17:14:31 -0600

    Create room handler, check required params

--- backend/app.js
+++ backend/app.js
@@ -14,10 +14,26 @@ let rooms = [
     //         {player_name: 'Mike', balance: 1500, secret: 'j18fh173n8'}
     //     ]
     // }
+
+
 ];
 
 io.on('connection', socket => {
+    socket.on('create-room', (args) => {
+        // Params:
+        // room_name: The name of the room the user wishes to create
+        // room_password: A password the user supplies that is required for players to join their room
+        // yourname: The display name this user wishes to have
 
+        if(
+            typeof args.room_name === 'undefined' ||
+            typeof args.room_password === 'undefined' ||
+            typeof args.yourname === 'undefined'
+        ) {
+            socket.emit('error', {message: 'Please only send the required parameters'});
+            return;
+        }
+    });
 });
 
 server.listen(8000);
