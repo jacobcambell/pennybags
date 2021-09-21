4b296e7a13c8d9486a1e3a475ffb5a8b66fb6914
Jacob Cambell <jacob@jacobcambell.com>
2021-09-21 12:24:24 -0600

    In JoinRoom, get list of rooms and save them in state

--- frontend/src/components/JoinRoom/index.js
+++ frontend/src/components/JoinRoom/index.js
@@ -1,8 +1,26 @@
 import styles from './JoinRoom.module.css';
+import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import io from 'socket.io-client';
 
 const JoinRoom = () => {
+
+    const socket = io('http://localhost:8000');
+
+    const [roomList, setRoomList] = useState([]);
+
+    useEffect(() => {
+        socket.emit('list-rooms');
+    }, []);
+
+    socket.on('room-list', (data) => {
+        setRoomList(data);
+    })
+
     return (
-        <div>join room</div>
+        <div className="page">
+            <p className="title">Join Room</p>
+        </div>
      );
 }
 
