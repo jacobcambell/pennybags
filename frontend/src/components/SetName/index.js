6946245ad0f081e336518c3b645acef4b2ec462a
Jacob Cambell <jacob@jacobcambell.com>
2021-09-23 12:01:43 -0600

    Basic SetName component styling

--- frontend/src/components/SetName/index.js
+++ frontend/src/components/SetName/index.js
@@ -3,7 +3,12 @@ import styles from './SetName.module.css';
 const SetName = () => {
     return (
         <div className="page">
-            <p>Set name</p>
+            <p className={styles.title}>Welcome to iMonopoly</p>
+            <p className={styles.desc}>To get started, please enter your display name.</p>
+
+            <input type="text" className={styles.field} placeholder="Your Name" />
+
+            <button className={styles.btn}>Let's Go</button>
         </div>
      );
 }
