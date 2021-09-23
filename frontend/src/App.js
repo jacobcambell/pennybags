98baffc3a344210acec0145ee9b28d5508edb305
Jacob Cambell <jacob@jacobcambell.com>
2021-09-23 11:54:53 -0600

    Create SettingsContext and pass a settings state to provider

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,37 @@
+import { useState } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
 import CreateRoom from './components/CreateRoom';
 import Home from './components/Home';
 import JoinRoom from './components/JoinRoom';
 import Nav from './components/Nav';
 import SetName from './components/SetName';
 
+import { SettingsContext } from './SettingsContext';
+
 function App() {
+
+  const [settings, setSettings] = useState({ name: null });
+
   return (
     <div className="App">
-      <Router>
-        <Nav></Nav>
+      <SettingsContext.Provider value={{settings, setSettings}}>
+        <Router>
+          <Nav></Nav>
 
-        <Switch>
-          <Route exact path="/">
-            <SetName></SetName>
-          </Route>
-          <Route exact path="/create-room">
-            <CreateRoom></CreateRoom>
-          </Route>
-          <Route exact path="/join-room">
-            <JoinRoom></JoinRoom>
-          </Route>
-        </Switch>
-      </Router>
+          <Switch>
+            <Route exact path="/">
+              <SetName></SetName>
+            </Route>
+            <Route exact path="/create-room">
+              <CreateRoom></CreateRoom>
+            </Route>
+            <Route exact path="/join-room">
+              <JoinRoom></JoinRoom>
+            </Route>
+          </Switch>
+        </Router>
+      </SettingsContext.Provider>
     </div>
   );
 }
