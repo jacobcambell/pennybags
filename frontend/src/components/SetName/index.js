7dcff3d4f6a0c00bacc9c185181cd03e88650a51
Jacob Cambell <jacob@jacobcambell.com>
2021-09-23 12:13:22 -0600

    Grab state from context, and update the state based on SetName's input value

--- frontend/src/components/SetName/index.js
+++ frontend/src/components/SetName/index.js
@@ -1,14 +1,25 @@
 import styles from './SetName.module.css';
+import { useContext, useState } from 'react';
+import { SettingsContext } from '../../SettingsContext';
 
 const SetName = () => {
+
+    const { settings, setSettings } = useContext(SettingsContext);
+
+    const updateName = () => {
+        setSettings({...settings, name: name});
+    }
+
+    const [name, setName] = useState('');
+
     return (
         <div className="page">
             <p className={styles.title}>Welcome to iMonopoly</p>
             <p className={styles.desc}>To get started, please enter your display name.</p>
 
-            <input type="text" className={styles.field} placeholder="Your Name" />
+            <input onChange={(e) => { setName(e.target.value) }}type="text" className={styles.field} placeholder="Your Name" />
 
-            <button className={styles.btn}>Let's Go</button>
+            <button onClick={updateName} className={styles.btn}>Let's Go</button>
         </div>
      );
 }
