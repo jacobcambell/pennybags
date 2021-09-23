24f6dd94e51731a69a32f387b02664a4c829e5c5
Jacob Cambell <jacob@jacobcambell.com>
2021-09-23 12:22:24 -0600

    Show current user's name in nav if it exists

--- frontend/src/components/Home/index.js
+++ frontend/src/components/Home/index.js
@@ -2,7 +2,6 @@ import styles from './Home.module.css';
 import { Link } from 'react-router-dom';
 
 const Home = () => {
-
     return (
         <div className="page">
             <p className={styles.title}>Welcome to iMonopoly</p>
--- frontend/src/components/Nav/index.js
+++ frontend/src/components/Nav/index.js
@@ -1,10 +1,18 @@
 import { Link } from 'react-router-dom';
 import styles from './Nav.module.css';
-
+import { useContext } from 'react';
+import { SettingsContext } from '../../SettingsContext';
 const Nav = () => {
+
+    const { settings, setSettings } = useContext(SettingsContext);
+
     return (
         <div className={styles.nav}>
             <Link to="/" className={styles.logo}>iMonopoly</Link>
+            {
+                settings.name &&
+                <p className={styles.user}>Playing as: {settings.name}</p>
+            }
         </div>
      );
 }
