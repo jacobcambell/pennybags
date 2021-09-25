f610a2dc20e2b8fae2a367d279111a649f35afc4
Jacob Cambell <jacob@jacobcambell.com>
2021-09-25 12:30:40 -0600

    Basic JoinRoom functionality

--- frontend/src/components/JoinRoom/index.js
+++ frontend/src/components/JoinRoom/index.js
@@ -1,13 +1,17 @@
-import styles from './JoinRoom.module.css';
-import { Link } from 'react-router-dom';
-import { useEffect, useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
+import { useEffect, useState, useContext } from 'react';
 import io from 'socket.io-client';
+
+import styles from './JoinRoom.module.css';
+
+import { SettingsContext } from '../../SettingsContext';
 
 const JoinRoom = () => {
 
     const socket = io('http://localhost:8000');
-
     const [roomList, setRoomList] = useState([]);
+    const { settings, setSettings } = useContext(SettingsContext);
+    const history = useHistory();
 
     useEffect(() => {
         socket.emit('list-rooms');
@@ -17,19 +21,39 @@ const JoinRoom = () => {
         setRoomList(data);
     })
 
+    const handleJoin = (room_name) => {
+        socket.emit('join-room', {
+            player_name: settings.player_name,
+            room_name: room_name
+        })
+    }
+
+    socket.on('success', (data) => {
+        // Update the state with their secret and room name
+        setSettings({
+            ...settings,
+            room_name: data.room_name,
+            secret: data.secret
+        });
+
+        history.push('/game');
+    })
+
     return (
         <div className="page">
             <p className="title">Join Room</p>
 
             {
-                roomList && roomList.map((room, index) => (
-                    <Link key={index} to="/" className={styles.roomTile}>
-                        {room}
-                    </Link>
+                roomList && roomList.map((room, i) => (
+                    <div onClick={() => { handleJoin(room.room_name) }} key={i} className={styles.roomTile}>
+                        {room.room_name}
+                    </div>
                 ))
             }
+
+            <p>Joining room as {settings.player_name} </p>
         </div>
-     );
+    );
 }
 
 export default JoinRoom;
