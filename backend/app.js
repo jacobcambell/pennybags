e6a0ec97426b079ca1c695e36c1d263b6d9b94a6
Jacob Cambell <jacob@jacobcambell.com>
2021-09-25 10:49:41 -0600

    get-event-feed handler

--- backend/app.js
+++ backend/app.js
@@ -25,8 +25,8 @@ io.on('connection', socket => {
         }
 
         // Check if a room with that name already exists
-        for(let i = 0; i < rooms.length; i++){
-            if(rooms[i].room_name === args.room_name){
+        for (let i = 0; i < rooms.length; i++) {
+            if (rooms[i].room_name === args.room_name) {
                 socket.emit('error', {
                     message: 'Room with that name already exists'
                 });
@@ -47,7 +47,8 @@ io.on('connection', socket => {
                 { player_name: args.yourname, balance: 1500, secret: SECRET }
             ],
             event_feed: [
-                'Room created by ' + args.yourname
+                'Room created by ' + args.yourname,
+                'Somebody died'
             ]
         };
 
@@ -55,7 +56,7 @@ io.on('connection', socket => {
         rooms.push(newRoom);
 
         // Room was created successfully, we want to send the user their secret that is attached to their player in the new room, as well as the room name
-        socket.emit('success', {secret: SECRET, room_name: args.room_name});
+        socket.emit('success', { secret: SECRET, room_name: args.room_name });
     });
 
     socket.on('list-rooms', (args) => {
@@ -63,10 +64,31 @@ io.on('connection', socket => {
         let listOfRooms = [];
 
         rooms.map((room) => {
-            listOfRooms.push({room_name: room.room_name, owner: room.owner});
+            listOfRooms.push({ room_name: room.room_name, owner: room.owner });
         });
 
         socket.emit('room-list', listOfRooms);
+    })
+
+    socket.on('get-event-feed', (args) => {
+        if (typeof args.room_name === 'undefined') {
+            socket.emit('error', {
+                message: 'Please specify a room name'
+            });
+        }
+
+        // Get event feed for the requested room name
+        // Loop through all the rooms, and see if the room name matches. If so, return that object's event feed
+        let event_feed = [];
+
+        for (let i = 0; i < rooms.length; i++) {
+            if(rooms[i].room_name === args.room_name){
+                event_feed = rooms[i].event_feed;
+            }
+        }
+
+        // Send the event feed back to the client
+        socket.emit('send-event-feed', event_feed);
     })
 });
 
