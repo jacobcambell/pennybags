db029708035b17d3e4c562c1acaf5e218fc1f50c
Jacob Cambell <jacob@jacobcambell.com>
2021-09-25 12:09:40 -0600

    Display players/balances in PlayerList

--- frontend/src/components/Game/PlayerList/index.js
+++ frontend/src/components/Game/PlayerList/index.js
@@ -1,4 +1,4 @@
-import { useEffect, useContext } from 'react';
+import { useEffect, useContext, useState } from 'react';
 import styles from './PlayerList.module.css';
 import io from 'socket.io-client';
 
@@ -8,18 +8,26 @@ const PlayerList = () => {
 
     const socket = io('http://localhost:8000');
     const { settings, setSettings } = useContext(SettingsContext);
+    const [players, setPlayers] = useState();
 
     useEffect(() => {
         socket.emit('list-players-in-room', { room_name: settings.room_name });
     }, []);
 
     socket.on('send-player-list', (data) => {
-        console.log(data)
+        setPlayers(data);
     })
 
     return (
         <div className={styles.playerlist}>
             <p className={styles.title}>Players in Room:</p>
+
+            {
+                players &&
+                players.map((player, i) => (
+                    <p key={i} className={styles.player}>{player.player_name} : ${player.balance}</p>
+                ))
+            }
         </div>
     );
 }
