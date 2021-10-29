f6ab7e03a509759dd60c67fe3632effea2e559cd
Jacob Cambell <jacob@jacobcambell.com>
2021-10-29 15:44:49 -0600

    Import react navigation modules

--- app/App.tsx
+++ app/App.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import SocketProvider from './src/contexts/SocketProvider';
 import Lander from './src/pages/Lander';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <SocketProvider>
-      <Lander></Lander>
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="Lander" component={Lander} />
+        </Stack.Navigator>
+      </NavigationContainer>
+
     </SocketProvider>
   );
 }
--- app/src/pages/Lander.tsx
+++ app/src/pages/Lander.tsx
@@ -1,15 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Image, SafeAreaView, Text, StyleSheet, TextInput, Pressable } from 'react-native';
+import { socket } from '../components/socket';
 import { Theme } from '../components/Theme';
 
 const Lander = () => {
+
+    const [name, setName] = useState('');
+
+    const handleContinue = () => {
+        socket.emit('create-user', {
+            username: name
+        })
+    }
+
     return (
         <SafeAreaView style={styles.content}>
             <Image source={require('../../assets/logo-small.png')} style={styles.logo}></Image>
             <Text style={styles.title}>Pennybags</Text>
 
-            <TextInput style={styles.input} placeholder={'Enter Your Name'}></TextInput>
-            <Pressable style={styles.button}>
+            <TextInput onChangeText={(e) => { setName(e) }} style={styles.input} placeholder={'Enter Your Name'}></TextInput>
+            <Pressable onPress={handleContinue} style={styles.button}>
                 <Text style={styles.buttonText}>Continue</Text>
             </Pressable>
         </SafeAreaView>
