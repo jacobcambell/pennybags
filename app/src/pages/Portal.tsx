4fb4e38516760b4ef5889a9bf628767718326909
Jacob Cambell <jacob@jacobcambell.com>
2021-10-29 20:22:17 -0600

    Show Portal page after username is set

---
+++ app/src/pages/Portal.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { SafeAreaView, Text } from 'react-native';
+
+const Portal = () => {
+    return (
+        <SafeAreaView>
+            <Text>Portal Page</Text>
+        </SafeAreaView>
+    )
+}
+
+export default Portal;
--- app/src/providers/UserProvider.tsx
+++ app/src/providers/UserProvider.tsx
@@ -2,8 +2,8 @@ import React, { createContext, useState } from 'react'
 import Lander from '../pages/Lander';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { Text } from 'react-native';
 import { UserContext } from '../contexts/UserContext';
+import Portal from '../pages/Portal';
 
 const Stack = createNativeStackNavigator();
 
@@ -21,7 +21,7 @@ const UserProvider = ({ children }) => {
                             <Stack.Screen name="Lander" component={Lander} />
                         </Stack.Navigator>
                     </NavigationContainer> :
-                    <Text>You have a username! {username}</Text>
+                    <Portal></Portal>
             }
         </UserContext.Provider>
     );
